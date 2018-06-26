5c1fc464b5a072dd445dba46827435a2f4fb1d4d
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2018-06-26 00:37:41 -0300

    Completed add_node method, corrected some references, and updated Node

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -5,6 +5,8 @@ class Node {
     public:
         Node(int v) {
             value = v;
+            left = NULL;
+            right = NULL;
         }
         int value;
         Node *left;
@@ -15,32 +17,56 @@ Node root(51);
 
 void add_node(Node *new_node) {
     Node *current_node = &root;
-    
-    while(current_node == NULL) {
+
+    while(current_node != NULL) {
         if(new_node->value < current_node->value) {
             if(current_node->left != NULL){
                 current_node = current_node->left;
             }
+            else{
+            	current_node->left = new_node;
+            	break;
+            }
         }
         else if(new_node->value > current_node->value) {
-            
+        	if(current_node->right != NULL){
+        		current_node = current_node->right;
+        	}
+            else{
+            	current_node->right = new_node;
+            	break;
+            }
         }
         else if(new_node->value < current_node->value) {
-            cout << "NÃ³ idÃªntico jÃ¡ encontrado!";
+            cout << "Nó idêntico já encontrado!";
         }
     }
 }
 
-main() {
-   cout << root.value;
-   Node esquerdo(21);
-   root.left = &esquerdo;
-   cout << root.left->value;
-   //Node *esquerdo = new Node(21);
-   //mine.left = &esquerdo;
-   
-   //mine->left = Node esq(21);
-   //mine->left = new Node(21);
-   //mine = &root;
-   //cout << mine->value;
+int main() {
+	/*
+	cout << root.value << endl;
+	Node esquerdo(21);
+	root.left = &esquerdo;
+	cout << root.left->value << endl;
+	*/
+
+	add_node(new Node(10));
+
+	Node *direito = new Node(60);
+	add_node(direito);
+
+	direito = new Node(61);
+	add_node(direito);
+
+	/*
+   Node *esquerdo = new Node(21);
+   mine.left = &esquerdo;
+
+   mine->left = Node esq(21);
+   mine->left = new Node(21);
+   mine = &root;
+   cout << mine->value;
+	 */
+	return 0;
 }
